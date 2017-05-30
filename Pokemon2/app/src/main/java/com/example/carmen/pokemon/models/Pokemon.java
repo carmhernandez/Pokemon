f94a5291bebf9dfba90b1415868a63fa8b09b81a
carmhernandez <carmencita1284@hotmail.com>
2017-05-29 23:25:48 -0500

    proyecto pokemon

---
+++ Pokemon2/app/src/main/java/com/example/carmen/pokemon/models/Pokemon.java
@@ -0,0 +1,35 @@
+package com.example.carmen.pokemon.models;
+
+/**
+ * Created by OSCAR LASSO on 19/05/2017.
+ */
+
+public class Pokemon {
+
+    private int number;
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getNumber() {
+        String[] urlPartes = url.split("/");
+        return Integer.parseInt(urlPartes[urlPartes.length - 1]);
+    }
+
+
+}
